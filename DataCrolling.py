2c4a12908171fed1bff346920927a02bd000b1a0
jithhuP <dbswjdtjq1@naver.com>
2023-04-10 20:58:37 +0900

    로또 크롤링 기본 세팅하기 위해 기본 값으로 미리 넣어놓기 아직 미완성으로 최대한 빠르게 한 페이지 크롤링을 완성할 예정

--- DataCrolling.py
+++ DataCrolling.py
@@ -1,25 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-raw = requests.get("https://tv.naver.com/r")
-html = BeautifulSoup(raw.text,'html.parser')
-#파이썬 소스코드가 실제 웹페이지 상으로는 어떤 것을 의미하는지 서로
-#서로 연결해서 이해하는 것이 매우 중요하다. 모습이라는 그림과 실제 웹페이지에서의
-#모습을 캡쳐하여 서로를 비교하였으니 이를 서로 비교하면서 이해하는 것이 매우 중요하고 그것을 잘 인식해야한다.
 
-#1 컨테이너 Container 데이터 수집
-container = html.select("div.inner")
-print(container)
-print(container[0])
-#2 컨테이너 Container 내부 데이터 수집
-title = container[0].select_one('dt.title')
-like = container[0].select_one('span_like')
-print(title.text.strip())
-print(like.text.strip())
+url = "https://www.dhlottery.co.kr/gameResult.do?method=byWin"
+html = requests.get(url).text
+soup = BeautifulSoup(html, 'html.parser')
 
+winning_numbers = []
 
+# 로또 당첨번호 추출
+for i in range(1, 7):
+    num = soup.select(f"div.win_result > div > ul > li:nth-child({i}) > span")[0].text
+    winning_numbers.append(num)
 
-#BS4
-#https://m.blog.naver.com/piry777/221696394407
-#requests URL
-#https://me2nuk.com/Python-requests-module-example/#:~:text=requests%20%EB%AA%A8%EB%93%88%EC%9D%B4%EB%9E%80%3F%20requests%EB%8A%94%20python%EC%82%AC%EC%9A%A9%EC%9E%90%EB%93%A4%EC%9D%84%20%EC%9C%84%ED%95%B4%20%EB%A7%8C%EB%93%A4%EC%96%B4%EC%A7%84%20%EA%B0%84%EB%8B%A8%ED%95%9C%20Python%EC%9A%A9,python%20%EC%93%B0%EB%A9%B4%EC%84%9C%20requests%20%EB%AA%A8%EB%93%88%EC%9D%84%20%EC%93%B0%EA%B2%8C%20%EB%90%98%EB%8A%94%20%EA%B2%BD%EC%9A%B0%EA%B0%80%20%EB%A7%8E%EC%9D%80%EB%8D%B0
-#https://medium.com/@mjhans83/%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%9C%BC%EB%A1%9C-%ED%81%AC%EB%A1%A4%EB%A7%81-%ED%95%98%EA%B8%B0-908e78ee09e0
+# 보너스 번호 추출
+bonus_num = soup.select("div.win_result > div > div > span")[0].text
+
+print("로또 당첨번호:", winning_numbers)
+print("보너스 번호:", bonus_num)
