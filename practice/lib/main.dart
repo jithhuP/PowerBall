239fdeef2d179a6fe5cbd3ece2a86aebf78169aa
1Parksanghyun <nks168810@gmail.com>
2023-04-29 08:41:38 +0900

    시간, 알림, 메뉴 구역 추가

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,19 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Main manu(임시)"),
-        ),
-        body: Center(
-          child: Text('Hello, World!'),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15.0,
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: const [
+                  SizedBox(
+                    height: 35.0,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: const [
+                          SizedBox(
+                            width: 271.0,
+                            child: Text(
+                              "시간",
+                              style: TextStyle(fontSize: 24.0),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Icon(
+                        Icons.add_alert_outlined,
+                        size: 55.0,
+                      ),
+                      Icon(
+                        Icons.menu,
+                        size: 55.0,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
