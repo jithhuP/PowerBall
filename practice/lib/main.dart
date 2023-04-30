85ad86ab6165fd8e71ba207dc0f0dda5e00b3b17
1Parksanghyun <nks168810@gmail.com>
2023-04-30 15:28:43 +0900

    타이머 기능 구현 및 주석 처리

--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 void main() {
   runApp(const App());
@@ -12,12 +13,14 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
+        // 좌우 빈 공간을 넣음
         body: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: 2.0,
           ),
           child: Column(
             children: [
+              // 최상단 빈공간 넣음
               const SizedBox(
                 height: 35.0,
               ),
@@ -27,6 +30,7 @@ class App extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      //시간 영역 테두리
                       Container(
                         height: 59.0,
                         alignment: Alignment.center,
@@ -36,12 +40,22 @@ class App extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
-                        child: const SizedBox(
+                        //시간 영역
+                        child: SizedBox(
                           width: 285.4,
-                          child: Text(
-                            "시간",
-                            style: TextStyle(fontSize: 24.0),
-                            textAlign: TextAlign.center,
+                          child: TimerBuilder.periodic(
+                            const Duration(seconds: 1),
+                            builder: (context) {
+                              return Text(
+                                '${DateTime.now().year}년 ${DateTime.now().month}월 ${DateTime.now().day}일 ${DateTime.now().hour + 9}시 ${DateTime.now().minute}분',
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                ),
+                              );
+                            },
                           ),
                         ),
                       ),
@@ -50,6 +64,7 @@ class App extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
+                      //알림 영역 테두리
                       Container(
                         decoration: BoxDecoration(
                           border: Border.all(
@@ -57,12 +72,14 @@ class App extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
+                        //알림 영역
                         child: const Icon(
                           Icons.add_alert_outlined,
                           size: 55.0,
                           color: Colors.white,
                         ),
                       ),
+                      //메뉴 영역 테두리
                       Container(
                         decoration: BoxDecoration(
                           border: Border.all(
@@ -70,6 +87,7 @@ class App extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
+                        // 메뉴 영역
                         child: const Icon(
                           Icons.menu,
                           size: 55.0,
