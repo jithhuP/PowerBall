00cb4431dcd6717e01d5efb4ee47b900b5beb841
1Parksanghyun <nks168810@gmail.com>
2023-05-24 01:13:37 +0900

    알림 화면 디자인 완료

--- lib/alert_screen.dart
+++ lib/alert_screen.dart
@@ -9,10 +9,20 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: ftColor[1 + darkMode],
       appBar: AppBar(
+        backgroundColor: ftColor[2 + darkMode],
+        iconTheme: IconThemeData(
+          color: ftColor[0 + darkMode],
+        ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('알림'),
+          children: [
+            Text(
+              '알림',
+              style: TextStyle(
+                color: ftColor[0 + darkMode],
+                fontFamily: "KBOGothic",
+              ),
+            ),
           ],
         ),
       ),
