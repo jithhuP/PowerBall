ed1e493251eea86419abf69e93fa8b342759a4c2
jithhuP <dbswjdtjq1@naver.com>
2023-05-29 16:19:58 +0900

    로또 번호 관련 데이터 크롤링 완료

--- DataCrolling.py
+++ DataCrolling.py
@@ -1,7 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-
 #pip install firebase-admin // firebase download
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import firestore
+
+#서비스 계정 키 파일 경로
+cred = credentials.Certificate("C:/firebase/key.json")
+firebase_admin.initialize_app(cred, {
+  'projectId': 'powerball-2023',
+})
+firebase_database = firestore.client()
+
+count = 1
+# Adding_document
 
     # 웹 페이지에서 HTML 코드 가져오기
 def getWinningNumber(round_num: int):
@@ -19,40 +33,32 @@ def getWinningNumber(round_num: int):
     winning_chat = soup.find('p', {'class': 'desc'})
     winning_data.append(winning_chat.text)
 
-
 #로또 번호 데이터 추출
     for i in range(7):
         num = soup.select('p > span', {'class': ''})[i].text
         winning_numbers.append(num)
-        winning_data.append(num)
 
-#로또 데이터 출력
-    print(winning_data)
+    #크롤링 한 데이터 직접 넣기
+
+    doc_ref = firebase_database.collection(u'PowerBall_num').document(u'%d' % count)
+    doc_ref.set({
+     u'Day': {winning_data[0],winning_data[1]},
+     u'number': {winning_numbers[0],winning_numbers[0],winning_numbers[1],winning_numbers[2],winning_numbers[3],winning_numbers[4],winning_numbers[5],winning_numbers[6]}
+})
+
+#=====def 내용값 여기서 종료=====#
+
+Current_number = 1069 #현재 로또 최근 회차
+Search_number = 1 #찾고자 하는 번호
+Add_number = 0
 
 #getWinningNumber의 내용 값을 출력하는 반복문
-for i in range(1, 5):
+for i in range(Search_number, Current_number+1):
+    count = Search_number + Add_number
     getWinningNumber(i)
-
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from firebase_admin import firestore
-
-
-#서비스 계정 키 파일 경로
-cred = credentials.Certificate("C:/firebase/key.json")
-firebase_admin.initialize_app(cred, {
-  'projectId': 'powerball-2023',
-})
-firebase_database = firestore.client()
-
-#크롤링 한 데이터 직접 넣기
-doc_ref = firebase_database.collection(u'users').document(u'user01')
-doc_ref.set({
-    u'level': 20,
-    u'money': 700,
-    u'job': "knight"
-})
+    Add_number+=1
+    
+#<찾고자 하는 값 = Search_num의 값을 변경하기>
 
 
 #링크 데이터 추가하기.
