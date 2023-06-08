ce8551b75782fc9222db0973e743602bf53127e6
jithhuP <dbswjdtjq1@naver.com>
2023-06-08 22:35:02 +0900

    firebase 올리기 성공

--- IssueDataCrolling.py
+++ IssueDataCrolling.py
@@ -1,6 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import firestore
+
+#서비스 계정 키 파일 경로
+cred = credentials.Certificate("C:/firebase/key.json")
+firebase_admin.initialize_app(cred, {
+  'projectId': 'powerball-2023',
+})
+firebase_database = firestore.client()
+
 news_url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={}'
 query = input('검색할 키워드 입력 : ')
 query = query.replace(' ', '+')
@@ -14,9 +26,17 @@ print("검색 결과".format((query)))
 
 a_list = soup.find_all('a', {'class': 'news_tit'})
 
+count = 1
+
 for a in a_list :
     print("제목 : ", a.get('title'))
     print("링크 : ", a.get('href'))
+    doc_ref = firebase_database.collection(u'PowerBall_num').document(u'%d' % count)
+    count += 1
+    doc_ref.set({
+     u'제목': a.get('title'),
+     u'링크': a.get('href')
+})
 
     
 
