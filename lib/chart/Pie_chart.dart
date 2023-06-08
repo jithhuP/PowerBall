8be96042a3ec2fc8d133565a76a519e628cea604
1Parksanghyun <nks168810@gmail.com>
2023-06-08 20:10:05 +0900

    병합 충돌 수정 #2

--- lib/chart/Pie_chart.dart
+++ lib/chart/Pie_chart.dart
@@ -1,45 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:open_source_project/chart/total_pie_chart.dart';
-import 'package:open_source_project/chart/fifty_pie_chart.dart';
+import 'total_pie_chart.dart';
+import 'fifty_pie_chart.dart';
 
 class Pie_chart extends StatelessWidget {
   const Pie_chart({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        home:Scaffold(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
             body: Container(
-              child: ListView(
-                children: [
-                  Column(
-                    children: [
-                      Text('50 횟수 비율',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black
-                        ),
-                      ),
-                      PieChartSample1(),
-                      Text('총 횟수 비율',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black
-                        ),
-                      ),
-                      PieChartSample2(),
-
-                    ],
+          child: ListView(
+            children: [
+              Column(
+                children: const [
+                  Text(
+                    '50 횟수 비율',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
+                  PieChartSample1(),
+                  Text(
+                    '총 횟수 비율',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  PieChartSample2(),
                 ],
               ),
-            )
-        )
-    );
+            ],
+          ),
+        )));
   }
 }
