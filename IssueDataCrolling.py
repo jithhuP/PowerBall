fdaa4088e421f99fa5ee4a0f1066ee02950da424
jithhuP <dbswjdtjq1@naver.com>
2023-06-08 21:41:49 +0900

    선택하면서 기사를 크롤링하는 code

--- IssueDataCrolling.py
+++ IssueDataCrolling.py
@@ -1,29 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
+import requests # HTTP 요청처리를 위해 사용하는 모듈이다. 
+from bs4 import BeautifulSoup # HTML 및 XML 파일에서 원하는 데이터를 손쉽게 Parsing 할 수 있는 Python 라이브러리다.  
 
-    # 웹 페이지에서 HTML 코드 가져오기
-def getWinningNumber(round_num: int):
-    url = f"https://www.dhlottery.co.kr/happy.do?method=fundPressPrView&txtNo={round_num}"
-    html = requests.get(url).text
-
-    # HTML 코드를 파싱하여 필요한 데이터 추출
-    soup = BeautifulSoup(html, 'html.parser')
-    Title = []
-    Day = []
-    link = []
-    #로또 상세 데이터 추출 
+# # 보안뉴스 사이트내 기사 제목 추출 구간이다. # 
+url = 'https://www.boannews.com/media/o_list.asp' # 크롤링할 주소를 입력하는 구간이다. 
+response = requests.get(url) # HTTP 요청처리를 GET방식으로 response라는 변수한테 주는 구간이다. 
+html = response.text # 응답을 읽고 텍스트를 반환하는 구간이다. 
+soup = BeautifulSoup(html, 'html.parser') # 해당 값을 parser하는 구간이다. 
+value = soup.find_all('span',class_="news_txt") # 특정 값을 추출하는 구간이다. 
+count = 1 
+titlelist = [] 
+for _ in value: # 기사 제목만 추출 하는 구간이다. 
+    vlaue2 = _.get_text() 
+    print(count,end=". ") 
+    print(vlaue2,end="\n") 
+    titlelist.append(vlaue2) # 기사 제목을 추출해 따로 저장해두는 구간이다. 
+    count += 1 
     
-    Title_data = soup.select_one('.lt_title')
-    Title.append(Title.text)
-    Day_data = soup.select_one('.lt_date')
-    Day.append(Day.text)
-    link = url
-
-    print('제목:', Title)
-    print('등록일:', Day)
-    print('링크:', link)
-
-
+# 관심 기사를 선택 하는 구간이다 # 
+number = int(input("관심 기사의 번호를 입력하세요 : ")) 
+searchvalue = titlelist[number-1] # 관심 기사를, 제목을 저장해둔 곳에서 가져오는 구간이다.
 
 
 
