45cb517a22775ed2bd11396348c64c3e4bd6eee5
jithhuP <dbswjdtjq1@naver.com>
2023-06-08 22:32:04 +0900

    기본 틀 완성 firebase에 올릴 준비

--- IssueDataCrolling.py
+++ IssueDataCrolling.py
@@ -2,13 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 news_url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={}'
-
-query = input('검색 키워드 입력: ')
+query = input('검색할 키워드 입력 : ')
 query = query.replace(' ', '+')
 
 req = requests.get(news_url.format((query)))
 print("URL : ", news_url.format((query)))
-print(req.text)
+#print(req.text)
+
+soup = BeautifulSoup(req.text, 'html.parser')
+print("검색 결과".format((query)))
+
+a_list = soup.find_all('a', {'class': 'news_tit'})
+
+for a in a_list :
+    print("제목 : ", a.get('title'))
+    print("링크 : ", a.get('href'))
+
+    
+
 
 #크롤링 한 데이터 직접 넣기
 #doc_ref = firebase_database.collection(u'users').document(u'user01')
